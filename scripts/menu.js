2f333296bb186f111c59f5c9146f09c3fa3960b2
Mikisha-ap <mikisha.aryanna@gmail.com>
2024-04-23 16:35:17 +0200

    Restructured website

---
+++ scripts/menu.js
@@ -0,0 +1,26 @@
+﻿export function initialize(currentPage) {
+	const menuItems = [
+		{ name: "Home", href: "../home/index.html" },
+		{ name: "Blogs", href: "../blogs/blogs.html" },
+		{ name: "Designs", href: "../design/design.html" },
+		{ name: "Essays", href: "../essay/essay.html" },
+		{ name: "Profile", href: "../profile/profile.html" },
+		{ name: "Portfolio", href: "../portfolio/portfolio.html" }
+	];
+
+	var nav = document.querySelector("header > nav");
+	var ul = document.createElement("ul"); // the unordered list
+
+	for (let menuItem of menuItems) {
+		if (currentPage != menuItem.name) {
+			var li = document.createElement("li")
+			var a = document.createElement("a")
+			a.innerText = menuItem.name
+			a.setAttribute("href", menuItem.href)
+			li.appendChild(a)
+			ul.appendChild(li)
+		}
+	}
+
+	nav.appendChild(ul)
+}

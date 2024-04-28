3a7440640f86e245cb5218762293a3bd90cd9671
Mikisha-ap <mikisha.aryanna@gmail.com>
2024-04-29 00:28:44 +0200

    adding animations to the home page and adjusting some stuff

---
+++ scripts/menuAnimation.js
@@ -0,0 +1,38 @@
+
+function bigImg(imgId, linkId) {
+	var img = document.getElementById(imgId)
+	img.style.height = "300px";
+	img.style.width = "300px";
+	var imgRect = img.getBoundingClientRect();
+	setTimeout(rotateImage, 5);
+
+	var div = document.getElementById(linkId);
+	var divRect = div.getBoundingClientRect();
+	div.style.top = (imgRect.top + (imgRect.height - divRect.height) / 2) + 'px';
+	div.style.left = (imgRect.left + (imgRect.width - divRect.width) / 2) + 'px';
+	div.style.visibility = "visible";
+}
+
+function normalImg(imgId, linkId) {
+	var img = document.getElementById(imgId)
+	img.style.height = "200px";
+	img.style.width = "200px";
+	document.getElementById(linkId).style.visibility = "hidden";
+}
+
+var rotated = document.getElementById("blogIcon");
+
+let rotation = 0;
+const angle = 1;
+
+function rotateImage() {
+	if (rotated == null) { rotated = document.getElementById("blogIcon"); }
+	rotation = (rotation + angle) % 360;
+	rotated.style.transform = `rotate(${rotation}deg)`;
+	// setInterval(rotateImage, 100)
+	//if (onmouseover()) {
+	//  setTimeout(rotateImage)
+	//}
+}
+setInterval(rotateImage, 100)
+

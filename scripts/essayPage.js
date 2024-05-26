c77d3909e6a7f64506a05db112c1ae504e3f1508
Mikisha-ap <mikisha.aryanna@gmail.com>
2024-05-27 01:07:27 +0200

    adding in content and fixing essay page formatting

---
+++ scripts/essayPage.js
@@ -0,0 +1,21 @@
+const essayItems = [
+	"essay1.html"
+]
+
+let essayIndex = 0;
+
+function moveNext() {
+	if (essayIndex < essayItems.length - 1) {
+		essayIndex++;
+		let post = document.getElementById("essayFrame");
+		post.src = essayItems[essayIndex];
+	}
+}
+
+function moveBack() {
+	if (essayIndex > 0) {
+		essayIndex--;
+		let post = document.getElementById("essayFrame");
+		post.src = essayItems[essayIndex];
+	}
+}

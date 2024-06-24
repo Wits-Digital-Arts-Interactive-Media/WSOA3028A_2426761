fef9f4136962dc63b1a16b702b74eb254afb9ab9
Mikisha-ap <mikisha.aryanna@gmail.com>
2024-06-24 05:48:05 +0200

    sorting out menu and burger menu issues and fixing portfolio section

--- scripts/common.js
+++ scripts/common.js
@@ -1,11 +1,29 @@
 function toggleMenu() {
 	let chkBox = document.getElementById("chkBMenu");
 	if (chkBox != null) {
+		let nav = document.getElementById("navSpheres");
+		let childDivs = null;
+		let childDivsArray = null;
+		if (nav != null) {
+			childDivs = nav.querySelectorAll('div');
+			childDivsArray = Array.from(childDivs);
+		}
+
 		if (chkBox.checked) {
 			document.getElementById("navContainer").style.left = "0%";
+			if (childDivsArray != null) {
+				for (let i = 0; i < childDivsArray.length; i++) {
+					childDivsArray[i].style.zIndex = -1;
+				}
+			}
 		}
 		else {
 			document.getElementById("navContainer").style.left = "105%";
+			if (childDivsArray != null) {
+				for (let i = 0; i < childDivsArray.length; i++) {
+					childDivsArray[i].style.zIndex = 0;
+				}
+			}
 		}
 	}
 }
--- scripts/profile.js
+++ scripts/profile.js
@@ -4,7 +4,7 @@
 	const nextImage = images[1];
 
 	setTimeout(() => {
-		activeImage.src = "../images/profile/mikisha_with_speech_trans.png";
+		activeImage.src = "../images/profile/cartoonMikisha_with_speech_trans.png";
 		document.getElementById("profileDetails").style.visibility = "visible";
 	}, 1250); // Delay of 1.25 second before the transition
 });
